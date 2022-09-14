5f97f981b98bc1ec21768d102605abd8e4ba279e
hugo <hugo@astrek.net>
2022-09-14 23:03:47 +0300

    ex 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+    return (
+        <>
+            <button onClick={() => props.set(props.variable + 1)} >
+                {props.text}
+            </button>
+        </>
+    )
+}
+
+const StatisticLine = (props) => {
+    return (
+        <div>
+            {props.text} {props.value} {props.unit}
+        </div>
+    )
+}
+
 const Statistics = (props) => {
     if ( props.good === 0 && props.neutral === 0 && props.bad ===0) {
         return (
@@ -10,14 +28,14 @@ const Statistics = (props) => {
     }
     else {
         return (
-            <div>
-                good {props.good} <br/>
-                neutral {props.neutral} <br/>
-                bad {props.bad} <br/>
-                total {props.all} <br/>
-                average {props.avg} <br/>
-                positive {props.goodPer} %
-            </div>
+            <>
+                <StatisticLine text="good" value ={props.good} />
+                <StatisticLine text="neutral" value ={props.neutral} />
+                <StatisticLine text="bad" value ={props.bad} />
+                <StatisticLine text="total" value ={props.all} />
+                <StatisticLine text="average" value ={props.avg} />
+                <StatisticLine text="positive" value ={props.goodPer} unit="%" />
+            </>
         )
     }
 }
@@ -32,9 +50,9 @@ const App = () => {
     return (
         <div>
             <h3>give feedback</h3>
-            <button onClick={() => setGood(good + 1)}>good</button>
-            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-            <button onClick={() => setBad(bad + 1)}>bad</button>
+            <Button text="good" variable={good} set={setGood} />
+            <Button text="neutral" variable={neutral} set={setNeutral} />
+            <Button text="bad" variable={bad} set={setBad} />
             <h3>statistics</h3>
             <Statistics good={good} neutral={neutral} bad={bad} total={all} avg={avg} goodPer={goodPer} />
         
